b0adc8087c439d91a7d31a3b73b9ebb0999a0eb8
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-08-27 12:34:51 -0300

    Add `FamilyLoadUpdateTests` and version 2026

---
+++ RevitTest.FamilyLoad.Tests/FamilyLoadUpdateTests.cs
@@ -0,0 +1,37 @@
+﻿using Autodesk.Revit.DB;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace RevitTest.FamilyLoad.Tests
+{
+    public class FamilyLoadUpdateTests : Utils.OneTimeOpenDocumentTest
+    {
+        [TestCase("Family*")]
+        public void RevitTests_LoadFamily(string familyName)
+        {
+            var files = Directory.GetFiles("Family", $"{familyName}.rfa", SearchOption.AllDirectories);
+            var currentVersion = int.Parse(application.VersionNumber);
+            using (Transaction transaction = new Transaction(document))
+            {
+                transaction.Start("LoadFamily");
+                foreach (var file in files)
+                {
+                    var basicFileInfo = BasicFileInfo.Extract(file);
+                    var fileVersion = int.Parse(basicFileInfo.Format);
+                    if (fileVersion > currentVersion)
+                    {
+                        Console.WriteLine($"Skip {file} - {fileVersion}");
+                        continue;
+                    }
+                    Console.WriteLine($"LoadFamily: \t{file} - {fileVersion}");
+                    var family = FamilyUtils.LoadFamily(document, file);
+                }
+
+
+                transaction.Commit();
+            }
+        }
+    }
+
+}
